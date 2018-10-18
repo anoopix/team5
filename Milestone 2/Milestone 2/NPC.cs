f5799e7fb0b864d47b948de0aaea244122e3202e
anoopix <axr5239@rit.edu>
2018-10-18 17:58:01 -0400

    Anoop's Player and NPC class
    rjighhusgf

---
+++ Milestone 2/Milestone 2/NPC.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Milestone_2
+{
+    class NPC
+    {
+        // attributes
+        private int affection;
+        private Texture2D texture;
+        private Rectangle objectPos;
+
+        // properties
+        public int Affection
+        {
+            get
+            {
+                return affection;
+            }
+            set
+            {
+                affection = value;
+            }
+        }
+
+        public Texture2D Texture
+        {
+            get
+            {
+                return texture;
+            }
+            set
+            {
+                texture = value;
+            }
+        }
+
+        public Rectangle ObjectPos
+        {
+            get
+            {
+                return objectPos;
+            }
+            set
+            {
+                objectPos = value;
+            }
+        }
+
+        public int XPos
+        {
+            get
+            {
+                return objectPos.X;
+            }
+            set
+            {
+                objectPos.X = value;
+            }
+        }
+
+        public int YPos
+        {
+            get
+            {
+                return objectPos.Y;
+            }
+            set
+            {
+                objectPos.Y = value;
+            }
+        }
+
+        // constructor
+        public NPC(int x, int y, int width, int height)
+        {
+            x = objectPos.X;
+            y = objectPos.Y;
+            width = objectPos.Width;
+            height = objectPos.Height;
+        }
+
+        // constructor
+        public void Draw(SpriteBatch sBatch)
+        {
+            sBatch.Draw(Texture, ObjectPos, Color.White);
+        }
+    }
+}
---
+++ Milestone 2/Milestone 2/Player.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milestone_2
+{
+    class Player
+    {
+        // attributes
+        private int money;
+        private int strength;
+        private int intelligence;
+        private int charm;
+
+        // properties
+        public int Money
+        {
+            get
+            {
+                return money;
+            }
+            set
+            {
+                money = value;
+            }
+        }
+
+        public int Strength
+        {
+            get
+            {
+                return strength;
+            }
+            set
+            {
+                strength = value;
+            }
+        }
+
+        public int Intelligence
+        {
+            get
+            {
+                return intelligence;
+            }
+            set
+            {
+                intelligence = value;
+            }
+        }
+
+        public int Charm
+        {
+            get
+            {
+                return charm;
+            }
+            set
+            {
+                charm = value;
+            }
+        }
+
+        // methods
+        public void ChangeMoney(int num)
+        {
+            money += num;
+        }
+
+        public void ChangeStrength(int num)
+        {
+            strength += num;
+        }
+
+        public void ChangeIntelligence(int num)
+        {
+            intelligence += num;
+        }
+
+        public void ChangeCharm(int num)
+        {
+            charm += num;
+        }
+
+        // constructor
+        public Player(int money, int strength, int intelligence, int charm)
+        {
+            this.money = money;
+            this.strength = strength;
+            this.intelligence = intelligence;
+            this.charm = charm;
+        }
+    }
+}
